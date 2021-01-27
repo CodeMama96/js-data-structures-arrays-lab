e3faf6c9e53750df7b4b360a6bcb55eb458087e3
Alicia Weenum <weenum.alicia@gmail.com>
2021-01-27 12:49:52 -0500

    commit tree c58e95707955d9299a9386d69e9d5d584960b07f

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(name){
+    return drivers.push("Ralph")
+}
+function destructivelyPrependDriver (name){
+    return drivers.unshift("Bob")
+}
+
+function destructivelyRemoveLastDriver(){
+    return drivers.pop("Garfield")
+}
+
+function destructivelyRemoveFirstDriver(){
+    return drivers.shift("Milo")
+}
+function appendDriver(name){
+    return [...drivers, name];
+}
+
+function prependDriver(name){
+    return [name, ...drivers];
+}
+
+function removeLastDriver () {
+ return drivers.slice(0, drivers.length - 1);
+  }	
+
+  function removeFirstDriver(){
+    return drivers.slice(1)
+
+  }
+
